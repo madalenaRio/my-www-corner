019e66d826df281abeccbfdc6fd6c554b03d3576
madalenaRio <madalena.rio@gmail,com>
2022-08-31 14:35:57 +0200

    small change to welcom message

--- short-nibble/resources/views/home.blade.php
+++ short-nibble/resources/views/home.blade.php
@@ -5,7 +5,7 @@
 <section class="flex flex-wrap flex-row justify-center">
         <div class="flex flex-wrap flex-col items-center">
             <h1 class="text-3xl">Welcome to the Looney writers comunity!</h1>
-                <h3 class="text-xl"> or should we say bin... ahahah!</h3>
+                <h3 class="text-xl"> or should we say Looney bin... ahahah!</h3>
         </div>
 </section>
 
