7430e816922b2a8b2a5096093d99c6b07457f2b1
madalenaRio <madalena.rio@gmail,com>
2022-10-10 16:55:40 +0200

    styling changes

--- resources/views/components/madCornerHeader.blade.php
+++ resources/views/components/madCornerHeader.blade.php
@@ -8,7 +8,7 @@
       </a>
     </div>
 
-    <div class="flex-row flex-wrap justify-between items-center px-10 mt-6 md:flex md:flex-wrap md:-mx-4 md:mt-12">
+    <div class="flex-row flex-wrap justify-between px-10 mt-6 md:flex md:flex-wrap md:-mx-4 md:mt-12">
 
       <a class="basis-1/4 flex flex-col flex-wrap justify-center md:w-1/3 md:px-4 xl:px-6 mt-8 md:mt-0 
          m-2 my-3 px-6 py-2 border-double border-4 text-clip overflow-hidden text-center border-amber-400 text-xl text-gray cursor-pointer rounded-lg
@@ -18,7 +18,7 @@
         <p class="text-gray-600"><i>"I live in the Present. </br> The Future I do not Know. </br> The Past is no longer mine." </br>Fernando Pessoa</i></p>
       </a>
 
-      <a class="basis-1/4 flex flex-col flex-wrap justify-center md:w-1 md:px-4 xl:px-6 mt-8 md:mt-0 
+      <a class="basis-1/4 flex flex-col flex-wrap justify-center md:w-1/3 md:px-4 xl:px-6 mt-8 md:mt-0 
          m-2 my-3 px-6 py-2 border-double border-4 text-clip overflow-hidden text-center border-amber-400 text-xl text-gray cursor-pointer rounded-lg
          hover:bg-amber-100 hover:text-yellow-800 active:bg-amber-500" href="{{route('skills')}}
         ">
@@ -26,7 +26,7 @@
         <p class="text-gray-600">skills & languages</p>
       </a>
 
-      <a class="basis-1/4 flex flex-col flex-wrap justify-center md:w-1 md:px-4 xl:px-6 mt-8 md:mt-0 
+      <a class="basis-1/4 flex flex-col flex-wrap justify-center md:w-1/3 md:px-4 xl:px-6 mt-8 md:mt-0 
          m-2 my-3 px-6 py-2 border-double border-4 text-clip overflow-hidden text-center border-amber-400 text-xl text-gray cursor-pointer rounded-lg
          hover:bg-amber-100 hover:text-yellow-800 active:bg-amber-500" href="{{route('random')}}
         ">
