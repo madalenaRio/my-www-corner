82183d1be1750543bda787cae9e90570b944a1d9
madalenaRio <madalena.rio@gmail,com>
2022-10-10 16:53:49 +0200

    styling changes + add card+ pictures

--- resources/views/components/madCornerHeader.blade.php
+++ resources/views/components/madCornerHeader.blade.php
@@ -1,46 +1,41 @@
 <div class="header-2">
 
-  <div >
-    <div class="container px-4 mx-auto">
+  <div class="flex flex-col flex-wrap justify-center items-center px-4 mx-auto">
 
-      <div class="text-center max-w-2xl mx-auto">
-        <a href="{{route('welcome')}}">
-          <h1 class="text-3xl md:text-4xl font-medium mb-2">Mad Corner</h1>
-        </a>
-      </div>
+    <div class="text-center max-w-2xl mx-auto">
+      <a href="{{route('welcome')}}">
+        <h1 class="text-3xl md:text-4xl font-medium mb-2">Mad Corner</h1>
+      </a>
+    </div>
 
-      <div class="flex-row justify-between px-10 mt-6 md:flex md:flex-wrap md:-mx-4 md:mt-12">
+    <div class="flex-row flex-wrap justify-between items-center px-10 mt-6 md:flex md:flex-wrap md:-mx-4 md:mt-12">
 
-        <a class="md:w-1/3 md:px-4 xl:px-6 mt-8 md:mt-0 text-center
-        m-2 my-3 px-6 py-2 border-double border-4 border-amber-400 text-xl text-gray cursor-pointer rounded-lg
+      <a class="basis-1/4 flex flex-col flex-wrap justify-center md:w-1/3 md:px-4 xl:px-6 mt-8 md:mt-0 
+         m-2 my-3 px-6 py-2 border-double border-4 text-clip overflow-hidden text-center border-amber-400 text-xl text-gray cursor-pointer rounded-lg
          hover:bg-amber-100 hover:text-yellow-800 active:bg-amber-500" href="{{route('about')}}
         ">
-          
-          <h5 class="text-xl font-medium uppercase mb-4">About</h5>
-          <p class="text-gray-600"><i>"I live in the Present. </br> The Future I do not Know. </br> The Past is no longer mine." </br>Fernando Pessoa</i></p>
-        </a>
+        <h5 class="text-xl font-medium uppercase mb-4">About</h5>
+        <p class="text-gray-600"><i>"I live in the Present. </br> The Future I do not Know. </br> The Past is no longer mine." </br>Fernando Pessoa</i></p>
+      </a>
 
-        <a class="md:w-64 md:px-4 xl:px-6 mt-8 md:mt-0 text-center
-        m-2 my-3 px-6 py-2 border-double border-4 border-amber-400 text-xl text-gray cursor-pointer rounded-lg
+      <a class="basis-1/4 flex flex-col flex-wrap justify-center md:w-1 md:px-4 xl:px-6 mt-8 md:mt-0 
+         m-2 my-3 px-6 py-2 border-double border-4 text-clip overflow-hidden text-center border-amber-400 text-xl text-gray cursor-pointer rounded-lg
          hover:bg-amber-100 hover:text-yellow-800 active:bg-amber-500" href="{{route('skills')}}
         ">
-          <h5 class="text-xl font-medium uppercase mb-4">Full Stack Developer</h5>
-          <p class="text-gray-600">skills & languages</p>
-        </a>
+        <h5 class="text-xl font-medium uppercase mb-4">Full Stack Developer</h5>
+        <p class="text-gray-600">skills & languages</p>
+      </a>
 
-        <a class="md:w-64 md:px-4 xl:px-6 mt-8 md:mt-0 text-center
-        m-2 my-3 px-6 py-2 border-double border-4 border-amber-400 text-xl text-gray cursor-pointer rounded-lg
+      <a class="basis-1/4 flex flex-col flex-wrap justify-center md:w-1 md:px-4 xl:px-6 mt-8 md:mt-0 
+         m-2 my-3 px-6 py-2 border-double border-4 text-clip overflow-hidden text-center border-amber-400 text-xl text-gray cursor-pointer rounded-lg
          hover:bg-amber-100 hover:text-yellow-800 active:bg-amber-500" href="{{route('random')}}
         ">
-          <h5 class="text-xl font-medium uppercase mb-4">Random</h5>
-          <p class="text-gray-600">Other stuff I do</p>
-        </a>
-
-      
-
-      </div>
+        <h5 class="text-xl font-medium uppercase mb-4">Random</h5>
+        <p class="text-gray-600">Other stuff I do</p>
+      </a>
 
     </div>
+
   </div>
 
 </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,41 +10,122 @@
         <h1 class="text-3xl">Welcome to my corner!</h1>
         <h3>Snoop around ;)</h3>
     </div>
-    <div class="flex flex-wrap flex-col items-center">
+    <div class="flex flex-wrap flex-col justify-center items-center">
         <section>
-            <h1>
+            <h1 class="mb-3 text-3xl font-bold text-gray-900 dark:text-gray-400">
                 My projects:
             </h1>
         </section>
+        <h1 class="mb-3 text-2xl font-bold text-gray-900 dark:text-gray-400">Back-End Projects</h1>
         <section>
-            <section>
-                <h1>Back-End Projects</h1>
-                <h3><b>Laravel</b></h3>
-                <div class="flex-row justify-between items-center mx-5 my-5 px-5 py-3">
-                    <a class="m-2 my-3 px-6 py-2 border-double border-4 border-amber-400 text-xl text-gray cursor-pointer rounded-lg
-         hover:bg-amber-100 hover:text-yellow-800 active:bg-amber-500" href="{{route('home')}}">ShortNibbles</a>
 
-                </div>
-            </section>
-            <section>
-                <h1>Javascript Projects</h1>
-                <div class="flex-row justify-between items-center mx-5 my-5 px-5 py-3">
-                    <a class="m-2 my-3 px-6 py-2 border-double border-4 border-fuchsia-400 text-xl text-gray cursor-pointer rounded-lg
-         hover:bg-fuchsia-100 hover:text-fuchsia-800 active:bg-fuchsia-500" href="https://madalenasblackjackcorner.netlify.app">Black-Jack game</a>
-                    <a class="m-2 my-3 px-6 py-2 border-double border-4 border-blue-400 text-xl text-gray cursor-pointer rounded-lg
-         hover:bg-blue-100 hover:text-blue-800 active:bg-blue-500" href="https://madalenamemorygamecorner.netlify.app">Memory game</a>
-                    <a class="m-2 my-3 px-6 py-2 border-double border-4 border-emerald-400 text-xl text-gray cursor-pointer rounded-lg
-         hover:bg-emerald-100 hover:text-emerald-800 active:bg-emerald-500" href="https://rockpaperscissors-madalena.netlify.app">Rock Paper Scissors game</a>
-                </div>
-                <h1>API Projects</h1>
-                <div class="flex-row justify-between items-center mx-5 my-5 px-5 py-3">
-                    <a class="m-2 my-3 px-6 py-2 border-double border-4 border-rose-400 text-xl text-gray cursor-pointer rounded-lg
-         hover:bg-rose-100 hover:text-rose-800 active:bg-rose-500" href="https://todaysweatherbymadalenario.netlify.app">Another Weather App</a>
-                </div>
+            <h3><b>Laravel</b></h3>
+            <div class="flex flex-wrap flex-col items-center justify-around m-4">
 
-            </section>
+                <!-- card  -->
+                <div class="basis-1/4 flex-col items-center before:max-w-sm bg-white rounded-lg border border-gray-200 shadow-md m-4 dark:bg-gray-800 dark:border-gray-700">
+                    <a href="#">
+                        <img class="rounded-t-lg" src="/images/ShortNibbles.png" alt="caption of the title of the writers blog">
+                    </a>
+                    <div class="flex-col justify-center items-center p-5">
+                        <a href="{{route('home')}}">
+                            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Short Nibbles</h5>
+                        </a>
+                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">A blog Project for looney writers</p>
 
+                        <div class="flex-row justify-between items-center p-5">
+                            <a class="flex flex-wrap m-2 justify-center my-3 px-5 py-2 border-double border-4 border-amber-400 text-xl text-gray cursor-pointer rounded-lg hover:bg-amber-100 hover:text-yellow-800 active:bg-amber-500" href="{{route('home')}}">ShortNibbles</a>
+                        </div>
+                    </div>
+                </div>
+                <!-- end card -->
+            </div>
         </section>
+
+        <h1 class="mb-3 text-2xl font-bold text-gray-900 dark:text-gray-400">Javascript Projects</h1>
+
+        <section class="flex flex-row flex-wrap justify-around mx-4 my-4">
+
+            <!-- card  -->
+            <div class="basis-1/4 flex-col items-center max-w-sm bg-white rounded-lg border border-gray-200 shadow-md m-4 dark:bg-gray-800 dark:border-gray-700">
+                <a href="#">
+                    <img class="rounded-t-lg" src="/images/blackJack.png" alt="playing cards images">
+                </a>
+                <div class="flex-col justify-center items-center p-5">
+                    <a href="https://madalenasblackjackcorner.netlify.app">
+                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Black Jack Game</h5>
+                    </a>
+                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">For this project I used Javascript.</p>
+
+                    <div class="flex-row justify-between items-center p-5">
+                        <a class="flex flex-wrap m-2 justify-center my-3 px-5 py-2 border-double border-4 border-fuchsia-400 text-xl text-gray cursor-pointer rounded-lg hover:bg-fuchsia-100 hover:text-fuchsia-800 active:bg-fuchsia-500" href="https://madalenasblackjackcorner.netlify.app">Give it a try</a>
+                    </div>
+                </div>
+            </div>
+            <!-- end card -->
+
+            <!-- card  -->
+            <div class="basis-1/4 flex-col items-center max-w-sm bg-white rounded-lg border border-gray-200 shadow-md m-4 dark:bg-gray-800 dark:border-gray-700">
+                <a href="#">
+                    <img class="rounded-t-lg" src="/images/RockPaperScissors.png" alt="image of the clicking color button for each game element">
+                </a>
+                <div class="flex-col justify-center items-center p-5">
+                    <a href="https://rockpaperscissors-madalena.netlify.app">
+                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Rock Paper Scissors game</h5>
+                    </a>
+                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">For this project I used Javascript and was a lot of trouble to figure the logic. lots of if else here.</p>
+
+                    <div class="flex-row justify-between items-center p-5">
+                        <a class="flex flex-wrap m-2 justify-center my-3 px-5 py-2 border-double border-4 border-emerald-400 text-xl text-gray cursor-pointer rounded-lg hover:bg-emerald-100 hover:text-emerald-800 active:bg-emerald-500" href="https://rockpaperscissors-madalena.netlify.app">try your luck</a>
+                    </div>
+                </div>
+            </div>
+            <!-- end card -->
+
+            <!-- card  -->
+            <div class="basis-1/4 flex-col items-center max-w-sm bg-white rounded-lg border border-gray-200 shadow-md m-4 dark:bg-gray-800 dark:border-gray-700">
+                <a href="#">
+                    <img class="rounded-t-lg" src="/images/memoryApp.png" alt="caption image of the memory cards from the game">
+                </a>
+                <div class="flex-col justify-center items-center p-5 ">
+                    <a href="https://madalenamemorygamecorner.netlify.app">
+                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Memory Game</h5>
+                    </a>
+                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">For this project I followed a toturial.</p>
+                    <div class="flex-row justify-between items-center p-5">
+                        <a class="flex flex-wrap m-2 justify-center my-3 px-5 py-2 border-double border-4 border-blue-400 text-xl text-gray cursor-pointer rounded-lg hover:bg-blue-100" href="https://madalenamemorygamecorner.netlify.app">test your memory</a>
+                    </div>
+                </div>
+            </div>
+            <!-- end card -->
+        </section>
+
+        <h1 class="mb-3 text-2xl font-bold text-gray-900 dark:text-gray-400">API Projects</h1>
+
+        <div class="flex flex-row flex-wrap justify-around mx-4 my-4">
+
+            <!-- card  -->
+            <div class="basis-1/4 flex-col items-center max-w-sm bg-white rounded-lg border border-gray-200 shadow-md m-4 dark:bg-gray-800 dark:border-gray-700">
+                <a href="#">
+                    <img class="rounded-t-lg" src="/images/weatherApp.png" alt="caption image of the search bar for searching a city to display the weather">
+                </a>
+                <div class="flex-col flex-wrap justify-center items-center p-5">
+                    <a href="https://todaysweatherbymadalenario.netlify.app">
+                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Another Weather App to add to the pile</h5>
+                    </a>
+                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">This was a tricky one but I made it through.</p>
+                    <div class="flex-row justify-between items-center p-5">
+                        <a class="flex flex-wrap m-2 justify-center my-3 px-5 py-2 border-double border-4 border-rose-400 text-xl text-gray cursor-pointer rounded-lg hover:bg-rose-100 hover:text-rose-800 active:bg-rose-500" href="https://todaysweatherbymadalenario.netlify.app">get your weather</a>
+                    </div>
+                </div>
+            </div>
+            <!-- end card -->
+
+        </div>
+
+
+
+
     </div>
 </section>
 @endsection
