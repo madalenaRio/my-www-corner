721d04e6cbbdb74a88c0ac9abd94c057c1b89920
madalenaRio <madalena.rio@gmail,com>
2022-10-12 17:25:56 +0200

    testing git rebase  changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -60,7 +60,7 @@
                         Rock Paper Scissors game
                     </h5>
                     <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 md:max-w-xl">
-                        This project was atest to my capacity to use conditional statments and loops.
+                        This project was a test to my capacity to use conditional statments and loops. Almost got lost is a loop myself!
                     </p>
                 </div>
             </a>
@@ -76,7 +76,7 @@
                         Memory Game
                     </h5>
                     <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 md:max-w-xl">
-                        credit where credit is due:</br>
+                        Credit where credit is due:</br>
                         For this project I followed a toturial by "https://developedbyed.com/"
                     </p>
                 </div>
@@ -93,14 +93,14 @@
 
             <!-- card  -->
             <div class="w-3/5">
-            <a href="https://madalenamemorygamecorner.netlify.app" class="flex flex-col items-center m-6 min-w-96 pr-4 bg-teal-200 rounded-lg border shadow-md md:flex-row md:max-w-screen hover:bg-teal-500 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+            <a href="https://todaysweatherbymadalenario.netlify.app" class="flex flex-col items-center m-6 min-w-96 pr-4 bg-teal-200 rounded-lg border shadow-md md:flex-row md:max-w-screen hover:bg-teal-500 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
                 <img class="object-cover w-full h-96 rounded-t-lg md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="/images/weatherApp.png" alt="">
                 <div class="flex flex-col justify-between pl-8 p-4 leading-normal">
                     <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                         Another Weather App to add to the pile.
                     </h5>
                     <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 md:max-w-xl">
-                        This was a tricky one but I made it through.
+                      Get your weather!
                     </p>
                 </div>
             </a>
