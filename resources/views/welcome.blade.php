4c89e3a20bf0a3085763d294c7f80aaeab906ebf
madalenaRio <madalena.rio@gmail.com>
2023-10-21 16:10:40 +0200

    corrected intro text

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,8 +9,12 @@
     <div class="flex flex-wrap flex-col justify-center items-center">
 
         <h1 id="jumpHere" class="m-4 text-3xl font-bold text-gray-900 dark:text-gray-400">
-            Back End Projects
+           Work in Progress!
         </h1>
+        <h3>
+           Maybe I will turn this section into an illustration Gallery ...!
+           So many possibilities.... :)
+        </h3>
 
     </div>
 </section>
